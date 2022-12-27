a4c6804ac3940b855e7b13a252f9f43f87770292
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2022-12-27 21:10:44 +0100

    Moving implemented, starting with apple

--- Snake.js
+++ Snake.js
@@ -2,7 +2,94 @@ console.log('Snake.js loaded');
 const canvas = document.getElementById('canvas'); 
 const ctx = canvas.getContext('2d');
 
+let speed = 7;
+
+let tileCount = 20;
+let tileSize = canvas.width / tileCount - 2;
+let headX = 10;
+let headY = 10;
+
+let xVelocity = 0;
+let yVelocity = 0;
+
+let appleX = 5;
+let appleY = 5;
+
 function GameLoop(){
 
+   
+    clearScreen();
+    changeSnakePosition();
+    drawSnake();
+    drawApple();
+    setTimeout(GameLoop, 1000/speed);
+}
+
+//setTimeOut --> cause game will be quicker with time
+
+function clearScreen(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0, canvas.clientWidth, canvas.height);
 
 }
+
+function drawSnake(){
+
+    ctx.fillStyle = 'green';
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
+
+}
+
+function changeSnakePosition(){
+
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+function drawApple(){
+    ctx.fillStyle = 'red';
+    ctx.fillRect (appleX, appleY, tileSize, tileSize);
+}
+
+document.addEventListener("keydown", keyDown);
+
+function keyDown(event){
+    if(event.keyCode == 38){
+        if(yVelocity == 1){
+            return;
+        }
+        yVelocity = -1;
+        xVelocity = 0;
+
+    }
+
+    if(event.keyCode == 40){
+        if(yVelocity == -1){
+            return;
+        }
+        yVelocity = 1;
+        xVelocity = 0;
+
+    }
+
+    if(event.keyCode == 37){
+        if(xVelocity == 1){
+            return;
+        }
+        yVelocity = 0;
+        xVelocity = -1;
+
+    }
+
+    if(event.keyCode == 39){
+        if(xVelocity == -1){
+            return;
+        }
+        yVelocity = 0;
+        xVelocity = 1;
+
+    }
+}
+
+
+GameLoop();
